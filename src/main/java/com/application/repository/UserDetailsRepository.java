261656f0935793ba012c261fa6eadd2dd4819398
Chandrasekhar <chandra5356.sl@gmail.com>
2023-01-20 15:35:58 +0530

    Create user details entity and repository

---
+++ src/main/java/com/application/repository/UserDetailsRepository.java
@@ -0,0 +1,11 @@
+package com.application.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.application.entity.UserDetails;
+
+
+public interface UserDetailsRepository extends CrudRepository<UserDetails, Integer> {
+
+}
